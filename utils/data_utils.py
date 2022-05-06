6052633b4eb296d8dd0165fd404fb9afdf43816c
Orest Mykhaskiv <64860368+orestmy@users.noreply.github.com>
2022-05-06 14:01:08 +0100

    revert to wandb logging and refactoring - working version

---
+++ utils/data_utils.py
@@ -0,0 +1,19 @@
+from loader.loader import get_loader
+
+def get_dataset_info(DATASET_NAME):
+    if "ilab" in DATASET_NAME:
+        loader_new = get_loader("multi_attribute_loader_file_list_ilab")
+        file_list_root = "dataset_lists/ilab_lists/"
+        att_path = "dataset_lists/combined_attributes.p"
+        NUM_CLASSES = (6, 6, 6, 6)
+    elif "mnist_rotation" in DATASET_NAME:
+        loader_new = get_loader('multi_attribute_loader_file_list_mnist_rotation')
+        file_list_root = "dataset_lists/mnist_rotation_lists/"
+        att_path = "dataset_lists/combined_attributes.p"
+        NUM_CLASSES = (10, 10, 10, 10)
+    elif "rotation_model" in DATASET_NAME:
+        loader_new = get_loader("multi_attribute_loader_file_list")
+        file_list_root = "dataset_lists/biased_cars_lists/"
+        att_path = "data/biased_cars/att_dict_simplified.p"
+        NUM_CLASSES = (5, 5, 5, 5)
+    return loader_new, NUM_CLASSES, file_list_root, att_path
---
+++ utils/logger_utils.py
@@ -0,0 +1,69 @@
+import argparse
+import os
+import wandb
+
+
+def parse_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_name", type=str)
+    parser.add_argument("--num_epochs", type=int)
+    parser.add_argument("--batch_size", type=int)
+    parser.add_argument("--arch", type=str)
+    parser.add_argument("--experiment_out_name", type=str)
+    parser.add_argument("--save_file_suffix", type=str)
+    parser.add_argument("--start_checkpoint_path", type=str)
+    parser.add_argument("--task", type=str, default="combined")
+    parser.add_argument("--wandblog", type=int, default=0)
+    args = parser.parse_args()
+    return args
+
+def create_logging_folders(args):
+    def create_folder(fol):
+        if not os.path.isdir(fol):
+            os.mkdir(fol)
+
+    create_folder("outputs")
+    create_folder("outputs/%s" % args.experiment_out_name)
+    create_folder("outputs/%s/saved_models" % args.experiment_out_name)
+    create_folder("outputs/%s/accuracies" % args.experiment_out_name)
+    create_folder("outputs/%s/logs" % args.experiment_out_name)
+    create_folder("outputs/%s/losses" % args.experiment_out_name)
+
+def get_log_filehandle(args, train_file_name):
+    DATASET_NAME = args.dataset_name
+    NUM_EPOCHS = args.num_epochs
+    BATCH_SIZE = args.batch_size
+    ARCH = args.arch
+    SAVE_FILE_SUFFIX = args.save_file_suffix
+    TASK = args.task
+
+    if TASK == "combined":
+        SAVE_FILE = "outputs/%s/logs/%s_%s_%s_%s_%s_%s.out" % (
+            args.experiment_out_name,
+            train_file_name,
+            ARCH,
+            DATASET_NAME,
+            NUM_EPOCHS,
+            BATCH_SIZE,
+            SAVE_FILE_SUFFIX,
+        )
+    else:
+        SAVE_FILE = "outputs/%s/logs/%s_%s_%s_%s_%s_%s_%s.out" % (
+            args.experiment_out_name,
+            train_file_name,
+            TASK,
+            ARCH,
+            DATASET_NAME,
+            NUM_EPOCHS,
+            BATCH_SIZE,
+            SAVE_FILE_SUFFIX,
+        )
+    SAVE_HANDLER = open(SAVE_FILE, "w")
+    return SAVE_HANDLER
+
+def log_wandb(phase, epoch_loss, epoch_accs):
+    logdict = {}
+    logdict[phase + '_loss'] = epoch_loss.item()
+    for id, acc in enumerate(epoch_accs):
+        logdict[phase + '_acc{}'.format(id)] = acc
+    wandb.log(logdict)
